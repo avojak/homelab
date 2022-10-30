bdd3069ef6349b3c4775e27b0ef306781a925f81
Andrew Vojak <andrew.vojak@gmail.com>
2022-10-30 16:58:06 -0600

    Starting to add UnPoller but disabled due to potential for crashing UDM

--- ansible/ansible.mk
+++ ansible/ansible.mk
@@ -4,4 +4,4 @@ init: $(REQUIREMENTS_FILE)
 	ansible-galaxy install -r $< --force
 
 run:
-	ansible-playbook play.yml --vault-password-file .vault-password-file --limit homelab-app-01
+	ansible-playbook play.yml --vault-password-file .vault-password-file --limit homelab-mon
